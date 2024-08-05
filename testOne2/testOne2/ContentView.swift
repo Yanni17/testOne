733b838988b641a08a5d028ddf460ed299244b44
ioannispechlivanis <pechlivanis.i@web.de>
2024-08-05 13:37:42 +0200

    inital

---
+++ testOne2/testOne2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testOne2
+//
+//  Created by Ioannis Pechlivanis on 05.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testOne2/testOne2/testOne2App.swift
@@ -0,0 +1,17 @@
+//
+//  testOne2App.swift
+//  testOne2
+//
+//  Created by Ioannis Pechlivanis on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct testOne2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
